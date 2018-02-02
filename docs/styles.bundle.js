93ddc3985ab318745870b9c71e158e4433c1739e
giscafer <giscafer@outlook.com>
2018-02-02 17:07:26 +0800

    add header menu

--- docs/styles.bundle.js
+++ docs/styles.bundle.js
@@ -86,7 +86,7 @@ exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-b
 exports.i(__webpack_require__("../../../../css-loader/index.js?{\"sourceMap\":false,\"importLoaders\":1}!../../../../postcss-loader/index.js?{\"ident\":\"postcss\"}!../../../../bootstrap/dist/css/bootstrap.min.css"), "");
 
 // module
-exports.push([module.i, "/* You can add global styles to this file, and also import other style files */\n", ""]);
+exports.push([module.i, "/* You can add global styles to this file, and also import other style files */\n.ant-menu-dark {\n  background: #293c55 !important; }\n", ""]);
 
 // exports
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,18 @@
 import { NgModule, ModuleWithProviders } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
+import { NgZorroAntdModule } from "ng-zorro-antd";
 
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
+        NgZorroAntdModule,
     ],
     exports: [
         CommonModule,
         FormsModule,
+        NgZorroAntdModule,
     ]
 })
 export class SharedModule {
